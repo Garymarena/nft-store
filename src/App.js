a8964cb8e7b8136e7e4336f3c26560e0c96bd7cd
Garymarena <garym.arena@gmail.com>
2022-01-03 17:24:22 -0500

    List Data Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
 import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import PunkList from './components/PunkList';
 
 function App() {
+  const [punkListData, setPunkListData] = useState([])
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        'https://testnets-api.opensea.io/assets?assets_contract_address=0x0B0C1C7A3c6067104B9B8530BF0c9E7BB02E75De&order_direction=asc'
+      )
+      console.log(openseaData.data.assets)
+      setPunkListData(openseaData.data.assets)
+    }
+
+    return getMyNfts()
+  }, [])
+
   return (
     <div className='app'>
     <Header />
@@ -11,6 +28,7 @@ function App() {
       name={'Bandana Punk'} 
       traits={[{'value': 7}]} 
       image='https://ipfs.thirdweb.com/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu' />
+    <PunkList punkListData={punkListData} />
   </div>
   )
 }
